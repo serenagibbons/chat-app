66b2268efb0573ff2c1587f44f8798cc39560f7b
Serena Gibbons <serenalgibbons@gmail.com>
2020-03-27 17:02:24 -0400

    Add OnClickListener to profile image to change avatar

--- app/src/main/java/com/example/friendspace/ProfileActivity.java
+++ app/src/main/java/com/example/friendspace/ProfileActivity.java
@@ -1,15 +1,26 @@
 package com.example.friendspace;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.widget.ContentLoadingProgressBar;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.friendspace.Model.User;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -17,6 +28,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -27,7 +44,12 @@ public class ProfileActivity extends AppCompatActivity {
     private Toolbar mToolbar;
 
     private FirebaseUser mFirebaseUser;
-    private DatabaseReference mReference;
+    private DatabaseReference mDatabaseRef;
+    private StorageReference mStorage;
+
+    private static final int SELECT_IMAGE = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +70,14 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        // initialize Storage Reference
+        mStorage = FirebaseStorage.getInstance().getReference("uploads");
+        // initialize Firebase User
         mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        mReference = FirebaseDatabase.getInstance().getReference("Users").child(mFirebaseUser.getUid());
+        // initialize Database Reference
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Users").child(mFirebaseUser.getUid());
 
-        mReference.addValueEventListener(new ValueEventListener() {
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 User user = dataSnapshot.getValue(User.class);
@@ -69,5 +95,85 @@ public class ProfileActivity extends AppCompatActivity {
 
             }
         });
+
+        // change profile image when clicked
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open image from android photos
+                selectImage();
+            }
+        });
     }
+
+    // open image from android photos
+    public void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, SELECT_IMAGE);
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = this.getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage() {
+
+        if (imageUri != null) {
+            final StorageReference fileReference = mStorage.child(System.currentTimeMillis() + "." +getFileExtension(imageUri));
+            uploadTask = fileReference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(ProfileActivity.this, "task not successful",Toast.LENGTH_SHORT).show();
+                        throw task.getException();
+                    }
+                    return fileReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        // get url to the uploaded content
+                        Uri downloadFile = task.getResult();
+                        String stringUri = downloadFile.toString();
+
+                        // update user's profile image in database
+                        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Users").child(mFirebaseUser.getUid());
+                        HashMap<String, Object> hashMap = new HashMap<>();
+                        hashMap.put("imageURL", stringUri);
+                        mDatabaseRef.updateChildren(hashMap);
+                    }
+                    else {
+                        Toast.makeText(ProfileActivity.this, "Failed to upload new profile image", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(ProfileActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // if request code == SELECT_IMAGE, result code is okay and data is not null, upload image
+        if (requestCode == SELECT_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            if (uploadTask != null && uploadTask.isInProgress()) {
+                Toast.makeText(ProfileActivity.this, "Upload in progress", Toast.LENGTH_SHORT).show();
+            } else {
+                uploadImage();
+            }
+        }
+    }
+
 }
