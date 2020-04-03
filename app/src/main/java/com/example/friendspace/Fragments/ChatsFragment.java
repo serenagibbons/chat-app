cee0f34f6b5be8a1015b1ce4b99d3e4ec9237d97
Serena Gibbons <serenalgibbons@gmail.com>
2020-04-03 14:51:28 -0400

    Filter chats RecyclerView with SearchView

--- app/src/main/java/com/example/friendspace/Fragments/ChatsFragment.java
+++ app/src/main/java/com/example/friendspace/Fragments/ChatsFragment.java
@@ -3,6 +3,7 @@ package com.example.friendspace.Fragments;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -36,6 +37,8 @@ public class ChatsFragment extends Fragment {
     private FirebaseUser mFirebaseUser;
     private DatabaseReference mReference;
 
+    private SearchView mSearchView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -47,6 +50,20 @@ public class ChatsFragment extends Fragment {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         mUserList = new ArrayList<>();
+
+        mSearchView = view.findViewById(R.id.search_view);
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mAdapter.getFilter().filter(newText);
+                return true;
+            }
+        });
 
         // get the current user
         mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
